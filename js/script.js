75473ff05c0040c0a80fe176155a66e30d133524
amuMabirimise <awmabirimise@gmail.com>
2024-07-26 21:25:02 +0200

    fixing color

--- js/script.js
+++ js/script.js
@@ -89,13 +89,3 @@ var swiper = new Swiper('.swiper-container', {
   },
   loop: true,
 });
-
-function loader(){
-    document.querySelector('.loader-container').classList.add('fade-out');
-}
-
-function fadeOut(){
-    setInterval(loader, 3000);
-}
-
-window.onload = fadeOut;
